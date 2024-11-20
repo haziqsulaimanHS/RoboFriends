d85671770cdde14a5b892b424663c61b28accc03
haziqsulaimanHS <haziq1810441@gmail.com>
2024-11-21 03:15:10 +0800

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,124 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'John Wick',
+      username: 'johnwick',
+      email: 'johnwick@gmail.com'
+    },
+    {
+      id: 2,
+      name: 'John Cena',
+      username: 'johncena',
+      email: 'johncena@gmail.com'
+    },
+    {
+      id: 3,
+      name: 'The Rock',
+      username: 'therock',
+      email: 'therock@gmail.com'
+    },
+    {
+      id: 4,
+      name: 'Randy Orton',
+      username: 'randyorton',
+      email: 'randyorton@gmail.com'
+    },
+    {
+      id: 5,
+      name: 'Bruce Lee',
+      username: 'brucelee',
+      email: 'brucelee@gmail.com'
+    },
+    {
+      id: 6,
+      name: 'Chuck Norris',
+      username: 'chucknorris',
+      email: 'chucknorris@gmail.com'
+    },
+    {
+      id: 7,
+      name: 'Hulk Hogan',
+      username: 'hulkhogan',
+      email: 'hulkhogan@gmail.com'
+    },
+    {
+      id: 8,
+      name: 'Stone Cold Steve Austin',
+      username: 'stonecold',
+      email: 'stonecold@gmail.com'
+    },
+    {
+      id: 9,
+      name: 'The Undertaker',
+      username: 'undertaker',
+      email: 'undertaker@gmail.com'
+    },
+    {
+      id: 10,
+      name: 'Macho Man Randy Savage',
+      username: 'machoman',
+      email: 'machoman@gmail.com'
+    },
+    {
+      id: 11,
+      name: 'The Big Show',
+      username: 'bigshow',
+      email: 'bigshow@gmail.com'
+    },
+    {
+      id: 12,
+      name: 'Triple H',
+      username: 'tripleh',
+      email: 'tripleh@gmail.com'
+    },
+    {
+      id: 13,
+      name: 'Batista',
+      username: 'batista',
+      email: 'batista@gmail.com'
+    },
+    {
+      id: 14,
+      name: 'CM Punk',
+      username: 'cmpunk',
+      email: 'cmpunk@gmail.com'
+    },
+    {
+      id: 15,
+      name: 'Edge',
+      username: 'edge',
+      email: 'edge@gmail.com'
+    },
+    {
+      id: 16,
+      name: 'Kane',
+      username: 'kane',
+      email: 'kane@gmail.com'
+    },
+    {
+      id: 17,
+      name: 'Brock Lesnar',
+      username: 'brocklesnar',
+      email: 'brocklesnar@gmail.com'
+    },
+    {
+      id: 18,
+      name: 'Shawn Michaels',
+      username: 'shawnmichaels',
+      email: 'shawnmichaels@gmail.com'
+    },
+    {
+      id: 19,
+      name: 'Daniel Bryan',
+      username: 'danielbryan',
+      email: 'danielbryan@gmail.com'
+    },
+    {
+      id: 20,
+      name: 'Seth Rollins',
+      username: 'sethrollins',
+      email: 'sethrollins@gmail.com'
+    }
+  ];
+  
+  
